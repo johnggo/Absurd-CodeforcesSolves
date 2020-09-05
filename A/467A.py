751034e86a1d99f3781d9ad4688ec45b064f1802
John <65880991+JohnXdator@users.noreply.github.com>
2020-09-05 12:36:36 -0400

    Update 467A.py

--- A/467A.py
+++ A/467A.py
@@ -1,3 +1,10 @@
 # Coded by JohnXdator
-s = int(input())
-print(sum(eval(input().replace(' ','-')) < -1 for i in range(s)))
+n = int(input())
+s = 0
+for i in range(n):
+  v,c = map(int, input().split())
+  if v+1 < c:
+    s += 1
+  else:
+    s -= 0
+print(s) 
