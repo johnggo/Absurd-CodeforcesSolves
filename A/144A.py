a0f62c026043f3b1b21648be8fe87e51a442d212
John <65880991+JohnXdator@users.noreply.github.com>
2020-09-10 21:44:12 -0400

    Create 144A.py

---
+++ A/144A.py
@@ -0,0 +1,9 @@
+Code by Johnxdator
+a = int(input())
+l = list(map(int, input().split()))
+c = l.index(max(l))
+sol = l[::-1].index(min(l))
+if c < len(l) - sol - 1:
+  print(c + sol)
+else:
+  print(c + sol - 1)
