a0ea3b0aedde340e986c45ec903fb706908e0856
John <65880991+JohnXdator@users.noreply.github.com>
2020-09-11 13:20:56 -0400

    Create 344A.py

---
+++ A/344A.py
@@ -0,0 +1,6 @@
+# Code By JohnXdator
+# I don't know why, but this Code give me Time exceeded on test 7 (I tried this problem with a lot of another accepted submissions :( )
+n=input
+c=p=0
+for i in' '*int(n()):s=n();c+=s!=p;p=s
+print(c)
