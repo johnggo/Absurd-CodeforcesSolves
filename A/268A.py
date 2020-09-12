6d90e65cdfb5d0904b49a28283324a4245984041
John <65880991+JohnXdator@users.noreply.github.com>
2020-09-12 14:23:33 -0400

    Create 268A.py

---
+++ A/268A.py
@@ -0,0 +1,12 @@
+# Code by JohnXdator
+n = int(input())
+a = []
+b = []
+s = 0
+for i in range(n):
+	h, e = map(int,input().split())
+	a.append(h)
+	b.append(e)	
+for i in a:
+	s += b.count(i)
+print(s)
