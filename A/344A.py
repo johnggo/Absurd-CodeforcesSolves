3185155c4f16a195b80ce55d732bc6e60c65522a
John <65880991+JohnXdator@users.noreply.github.com>
2020-09-23 22:48:17 -0400

    Update 344A.py

--- A/344A.py
+++ A/344A.py
@@ -1,6 +1,4 @@
 # Code By JohnXdator
-# I don't know why, but this Code give me Time exceeded on test 7 (I tried this problem with a lot of another accepted submissions :( )
-n=input
-c=p=0
-for i in' '*int(n()):s=n();c+=s!=p;p=s
-print(c)
+p=f=-1
+for s in open(0):f+=p!=s;p=s
+print(f)
