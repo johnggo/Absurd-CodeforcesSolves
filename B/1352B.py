f623a4591e655dfb29b511d0fb81ca11982db90a
John <65880991+JohnXdator@users.noreply.github.com>
2020-10-26 21:00:19 -0400

    Create 1352B.py

---
+++ B/1352B.py
@@ -0,0 +1,14 @@
+# Code by JohnXdator
+t = int(input())
+for i in range(t):
+	n, k = map(int, input().split())
+	a = list(map(int, input().split()))
+	b = list(map(int, input().split()))
+	a.sort()
+	b.sort(reverse=True)
+	for i in range(k):
+		if a[i]>b[i]:
+			continue
+		else:
+			a[i] = b[i]
+	print(sum(a))
