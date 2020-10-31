6145f2f35dcc132357a77bdd01aae38ac4c4dbf7
John <65880991+JohnXdator@users.noreply.github.com>
2020-10-31 17:38:10 -0400

    Create 1367B.py

---
+++ B/1367B.py
@@ -0,0 +1,17 @@
+# Code by JohnXdator
+t = int(input())
+for j in range(t):
+	n = int(input())
+	a = list(map(int, input().split()))
+	sol = 0
+	al = 0
+	for i in range(n):
+		if i%2 != a[i]%2:
+			if i%2 == 0:
+				sol += 1
+			else:
+				al += 1
+	if sol != al:
+		print(-1) 
+	else:
+		print(sol)
