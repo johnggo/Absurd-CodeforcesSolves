e4c9f43c7388a8c7d7c594adb9dca4bd0e6262d1
John <65880991+JohnXdator@users.noreply.github.com>
2020-11-05 20:07:00 -0400

    Delete 1343B.py

--- A/1343B.py
+++
@@ -1,9 +0,0 @@
-# Code By JohnXdator
-for i in range(int(input())):
-    n = int(input())
-    if n % 4 == 0:
-        print('YES')
-        print(*range(2, n + 1, 2), end=' ')
-        print(*range(1, n - 1, 2), n + (n // 2 - 1))
-    else:
-        print('NO')
