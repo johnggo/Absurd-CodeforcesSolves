589224e762cf33f36b92cb71ad43779ae5225939
John <65880991+JohnXdator@users.noreply.github.com>
2020-11-07 12:50:14 -0400

    Create 1360B.py

---
+++ B/1360B.py
@@ -0,0 +1,8 @@
+# Code by JohnXdator
+t = int(input())
+for w in range(t):
+  n = int(input())
+  s = list(map(int, input().split()))
+  s.sort()
+  sol = [s[i+1]-s[i] for i in range(n-1)]
+  print(min(sol))
