6e516975e670fa9f630e5dc5994466649b0ac0e4
john <65880991+johnggo@users.noreply.github.com>
2021-06-07 22:18:25 -0400

    Update 141A.py

--- A/141A.py
+++ A/141A.py
@@ -1,7 +1,9 @@
-n = input()
-a = input()
-p = input()
-if sorted(n + a) == sorted(p):
-    print("YES")
+# Time: 310 ms
+# Memory: 12 KB
+inv = str(input())
+anf = str(input())
+sor = str(input())
+if sorted(inv + anf) == sorted(sor):
+	print('YES')
 else:
-    print("NO")
+	print('NO')
