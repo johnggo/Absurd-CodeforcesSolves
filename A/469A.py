34e657fe6baf0c4de74176b90932fb3b17aefd9f
john <65880991+johnggo@users.noreply.github.com>
2021-07-02 00:06:30 -0400

    Update 469A.py

--- A/469A.py
+++ A/469A.py
@@ -1,7 +1,12 @@
+# Time: 93 ms
+# Memory: 12 KB
 n = int(input())
-x = input().split()[1:]
-y = input().split()[1:]
-if len(set(x+y)) == n:
-  print("I become the guy.")
+apy = list(map(int, input().split()))
+apx = list(map(int, input().split()))
+apset = set(apy[1:] + apx[1:])
+# n*(n+1)//2 is 1 + 2 + 3... n
+# https://en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
+if sum(apset) == n*(n+1)//2:
+	print('I become the guy.')
 else:
-  print("Oh, my keyboard!")
+	print('Oh, my keyboard!')
