dd21b652fbe61368d187c8a5c880e887426a6895
john <65880991+johnggo@users.noreply.github.com>
2021-07-03 16:22:19 -0400

    Update 268A.py

--- A/268A.py
+++ A/268A.py
@@ -1,11 +1,12 @@
+# Time: 216 ms
+# Memory: 16 KB
 n = int(input())
-a = []
-b = []
-s = 0
+chi, cai = [], []
+ccount = 0
 for i in range(n):
-	h, e = map(int,input().split())
-	a.append(h)
-	b.append(e)	
-for i in a:
-	s += b.count(i)
-print(s)
+	hi, ai = map(int, input().split())
+	chi.append(hi)
+	cai.append(ai)
+for e in chi:
+	ccount += cai.count(e)
+print(ccount)
