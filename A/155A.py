9d28c26c397e1abff1d9881ee9e2cae53292c118
john <65880991+johnggo@users.noreply.github.com>
2021-07-08 12:07:20 -0400

    Update 155A.py

--- A/155A.py
+++ A/155A.py
@@ -1,7 +1,13 @@
+# Time: 186 ms
+# Memory: 1600 KB
 n = int(input())
-s=list(map(int,input().split()))
-c=0
-for i in range(1,n):
-    if (s[i]>max(s[0:i])) or (s[i] < min(s[0:i])):
-        c=c+1
+ni = list(map(int, input().split()))
+c = 0
+for i in range(1, n):
+	minv = min(ni[0:i])
+	mayv = max(ni[0:i])
+	if ni[i] > mayv:
+		c += 1
+	elif ni[i] < minv:
+		c += 1
 print(c)
